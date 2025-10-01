0cddbb44c8f74230497aff7aa1aeba0a2f197d81
0xBasset <0xbasset@gmail.com>
2025-10-01 16:45:00 -0300

    remove testnet address from constants

--- scripts/safeharbor/src/constants.js
+++ scripts/safeharbor/src/constants.js
@@ -1,6 +1,6 @@
 // ADDRESSES
 export const FACTORY_ADDRESS = "0x9d211CaC1ce390F676d1cB1D2Eb681410EC47E47"; // V1.1 from https://github.com/security-alliance/safe-harbor?tab=readme-ov-file#registry-addresses
-export const AGREEMENT_ADDRESS = "0x276c889f5312844c6d3df9ed6c742649f79f9a9d";
+export const AGREEMENT_ADDRESS = "__PLACEHOLDER__";
 export const MULTICALL_ADDRESS = "0x5e227AD1969Ea493B43F840cfF78d08a6fc17796"; // MULTICALL entry on Chainlog
 
 
