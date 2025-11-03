1ac0b38b03cff9bbf6180a65fd7809beaf0ee524
SidestreamStrongStrawberry <SASCoreUnit+StrongStrawberry@gmail.com>
2025-11-03 17:01:42 +0100

    Base spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -139,9 +139,9 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:         address(0xa1D35e93d5BfAF300e0327d73143529f19B3DA78), // populate with deployed spell if deployed
-            deployed_spell_created: 1761857135,          // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:   23692666,          // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:         address(0), // populate with deployed spell if deployed
+            deployed_spell_created: 0,          // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:   0,          // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:        prevSpells, // older spells to ensure are executed first
             office_hours_enabled:   true,       // true if officehours is expected to be enabled in the spell
             expiration_threshold:   30 days     // Amount of time before spell expires
