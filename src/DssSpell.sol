9cfeb558b6004596655424adaf4d640f44db5bb4
SidestreamStrongStrawberry <SASCoreUnit+StrongStrawberry@gmail.com>
2025-11-07 12:28:47 +0100

    fix: overwrite nextCastTime as virtual

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -17,9 +17,48 @@
 pragma solidity 0.8.16;
 
 import "dss-exec-lib/DssExec.sol";
-import "dss-exec-lib/DssAction.sol";
+import {DssExecLib} from "dss-exec-lib/DssExecLib.sol";
 // Note: code matches https://github.com/sky-ecosystem/wh-lz-migration/blob/17397879385d42521b0fe9783046b3cf25a9fec6/deploy/MigrationInit.sol
-import { MigrationInit } from "src/dependencies/wh-lz-migration/MigrationInit.sol";
+import {MigrationInit} from "src/dependencies/wh-lz-migration/MigrationInit.sol";
+
+abstract contract DssAction {
+
+    using DssExecLib for *;
+
+    // Modifier used to limit execution time when office hours is enabled
+    modifier limited {
+        require(DssExecLib.canCast(uint40(block.timestamp), officeHours()), "Outside office hours");
+        _;
+    }
+
+    // Office Hours defaults to true by default.
+    //   To disable office hours, override this function and
+    //    return false in the inherited action.
+    function officeHours() public view virtual returns (bool) {
+        return true;
+    }
+
+    // DssExec calls execute. We limit this function subject to officeHours modifier.
+    function execute() external limited {
+        actions();
+    }
+
+    // DssAction developer must override `actions()` and place all actions to be called inside.
+    //   The DssExec function will call this subject to the officeHours limiter
+    //   By keeping this function public we allow simulations of `execute()` on the actions outside of the cast time.
+    function actions() public virtual;
+
+    // Provides a descriptive tag for bot consumption
+    // This should be modified weekly to provide a summary of the actions
+    // Hash: seth keccak -- "$(wget https://<executive-vote-canonical-post> -q -O - 2>/dev/null)"
+    function description() external view virtual returns (string memory);
+
+    // Returns the next available cast time
+    function nextCastTime(uint256 eta) external virtual view returns (uint256 castTime) {
+        require(eta <= type(uint40).max);
+        castTime = DssExecLib.nextCastTime(uint40(eta), uint40(block.timestamp), officeHours());
+    }
+}
 
 contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
