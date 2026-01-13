cb0a72e8e57a5368a6550cddf1c82981fa68c60a
0xLaz3r <219995259+0xLaz3r@users.noreply.github.com>
2026-01-13 13:09:11 +0100

    feat: :sparkles: add spell actions

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -18,6 +18,24 @@ pragma solidity 0.8.16;
 
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
+import { GemAbstract } from "dss-interfaces/ERC/GemAbstract.sol";
+import { VestAbstract } from "dss-interfaces/dss/VestAbstract.sol";
+
+interface DaiUsdsLike {
+    function daiToUsds(address usr, uint256 wad) external;
+}
+
+interface VestedRewardsDistributionLike {
+    function distribute() external returns (uint256 amount);
+}
+
+interface GovernanceOAppSenderLike {
+    function setCanCallTarget(address _srcSender, uint32 _dstEid, bytes32 _dstTarget, bool _canCall) external;
+}
+
+interface StarGuardLike {
+    function plot(address addr_, bytes32 tag_) external;
+}
 
 contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
@@ -42,22 +60,76 @@ contract DssSpellAction is DssAction {
     //
     // uint256 internal constant X_PCT_RATE = ;
 
+    // ---------- Math ----------
+    uint256 internal constant MILLION   = 10 ** 6;
+    uint256 internal constant WAD       = 10 ** 18;
+    uint256 internal constant RAY       = 10 ** 27;
+
+    // ---------- Contracts ----------
+    address internal immutable DAI                              = DssExecLib.dai();
+    address internal immutable DAI_USDS                         = DssExecLib.getChangelogAddress("DAI_USDS");
+    address internal immutable MCD_SPOT                         = DssExecLib.spotter();
+    address internal immutable MCD_VEST_SKY_TREASURY            = DssExecLib.getChangelogAddress("MCD_VEST_SKY_TREASURY");
+    address internal immutable REWARDS_DIST_USDS_SKY            = DssExecLib.getChangelogAddress("REWARDS_DIST_USDS_SKY");
+    address internal immutable MCD_VEST_SPK_TREASURY            = DssExecLib.getChangelogAddress("MCD_VEST_SPK_TREASURY");
+    address internal immutable REWARDS_DIST_LSSKY_SPK           = DssExecLib.getChangelogAddress("REWARDS_DIST_LSSKY_SPK");
+    address internal immutable MCD_CLIP_GUNIV3DAIUSDC1_A        = DssExecLib.getChangelogAddress("MCD_CLIP_GUNIV3DAIUSDC1_A");
+    address internal immutable MCD_CLIP_CALC_GUNIV3DAIUSDC1_A   = DssExecLib.getChangelogAddress("MCD_CLIP_CALC_GUNIV3DAIUSDC1_A");
+    address internal immutable MCD_CLIP_GUNIV3DAIUSDC2_A        = DssExecLib.getChangelogAddress("MCD_CLIP_GUNIV3DAIUSDC2_A");
+    address internal immutable MCD_CLIP_CALC_GUNIV3DAIUSDC2_A   = DssExecLib.getChangelogAddress("MCD_CLIP_CALC_GUNIV3DAIUSDC2_A");
+    address internal immutable KEEL_SUBPROXY                    = DssExecLib.getChangelogAddress("KEEL_SUBPROXY");
+    address internal immutable LZ_GOV_SENDER                    = DssExecLib.getChangelogAddress("LZ_GOV_SENDER");
+    address internal immutable SPARK_STARGUARD                  = DssExecLib.getChangelogAddress("SPARK_STARGUARD");
+    address internal immutable GROVE_STARGUARD                  = DssExecLib.getChangelogAddress("GROVE_STARGUARD");
+    address internal immutable KEEL_STARGUARD                   = DssExecLib.getChangelogAddress("KEEL_STARGUARD");
+
+    // ---------- LayerZero ----------
+    uint32 internal constant SOL_EID = 30168;
+    // Note: base58 ALM1JSnEhc5PkNecbSZotgprBuJujL5objTbwGtpTgTd to hex conversion can be checked at https://emn178.github.io/online-tools/base58/decode/?input=ALM1JSnEhc5PkNecbSZotgprBuJujL5objTbwGtpTgTd&output_type=hex
+    bytes32 internal constant SVM_CONTROLLER = 0x8aadd66fe8f142fb55a08e900228f5488fcc7d73938bbce28e313e1b87da3624;
+    // Note: base58 BPFLoaderUpgradeab1e11111111111111111111111 to hex conversion can be checked at https://emn178.github.io/online-tools/base58/decode/?input=BPFLoaderUpgradeab1e11111111111111111111111&output_type=hex
+    bytes32 internal constant BPF_LOADER = 0x02a8f6914e88a1b0e210153ef763ae2b00c2b93d16c124d2c0537a1004800000;
+
+    // ---------- Wallets ----------
+    address internal constant AEGIS_D       = 0x78C180CF113Fe4845C325f44648b6567BC79d6E0;
+    address internal constant BLUE          = 0xb6C09680D822F162449cdFB8248a7D3FC26Ec9Bf;
+    address internal constant BONAPUBLICA   = 0x167c1a762B08D7e78dbF8f24e5C3f1Ab415021D3;
+    address internal constant CLOAKY_2      = 0x9244F47D70587Fa2329B89B6f503022b63Ad54A5;
+    address internal constant TANGO         = 0xB2B86A130B1EC101e4Aed9a88502E08995760307;
+    address internal constant SKY_STAKING   = 0x05c73AE49fF0ec654496bF4008d73274a919cB5C;
+
+    // ---------- Spark Proxy Spell ----------
+    address internal constant SPARK_SPELL      = 0x2cB9Fa737603cB650d4919937a36EA732ACfe963;
+    bytes32 internal constant SPARK_SPELL_HASH = 0x5fdec666ca088e84b1e330ce686b9b4bb84d01022c8de54529dc90cacfd56e37;
+
+    // ---------- Grove Proxy Spell ----------
+    address internal constant GROVE_SPELL       = 0x6772d7eaaB1c2e275f46B99D8cce8d470fA790Ab;
+    bytes32 internal constant GROVE_SPELL_HASH  = 0x62e0ddd487406519e23c4c6e26414e898c2442dd90365ee1a4a7cb188114e614;
+
+    // ---------- Keel Proxy Spell ----------
+    address internal constant KEEL_SPELL       = 0x10AF705fB80bc115FCa83a6B976576Feb1E1aaca;
+    bytes32 internal constant KEEL_SPELL_HASH  = 0xa231c2a3fa83669201d02335e50f6aa379a6319c5972cc046b588c08d91fd44d;
+
     function actions() public override {
         // ---------- Reduce USDS>SKY Emissions ----------
         // Forum: https://forum.sky.money/t/atlas-edit-weekly-cycle-proposal-week-of-2026-01-12/27606
         // Poll: https://vote.sky.money/polling/QmYmaVVc#poll-detail
 
         // Yank MCD_VEST_SKY_TREASURY stream ID 9
+        VestAbstract(MCD_VEST_SKY_TREASURY).yank(9);
 
         // Call VestedRewardsDistribution.distribute() on REWARDS_DIST_USDS_SKY
+        VestedRewardsDistributionLike(REWARDS_DIST_USDS_SKY).distribute();
 
         // ---------- Reduce LSSKY>SPK Emissions ----------
         // Forum: https://forum.sky.money/t/atlas-edit-weekly-cycle-proposal-week-of-2026-01-12/27606
         // Poll: https://vote.sky.money/polling/QmYmaVVc#poll-detail
 
         // Yank MCD_VEST_SPK_TREASURY stream ID 2
+        VestAbstract(MCD_VEST_SPK_TREASURY).yank(2);
 
         // Call VestedRewardsDistribution.distribute() on REWARDS_DIST_LSSKY_SPK
+        VestedRewardsDistributionLike(REWARDS_DIST_LSSKY_SPK).distribute();
 
         // ---------- Offboard GUNI Vaults ----------
         // Forum: https://forum.sky.money/t/guni-offboarding/27420
@@ -66,74 +138,119 @@ contract DssSpellAction is DssAction {
         // Update GUNIV3DAIUSDC1-A Parameters as follows:
 
         // Liquidation Ratio (mat): increase by 898 percentage points, from 102% to 1000%
+        // Note: We are using low level methods because DssExecLib only allows setting `mat < 1000%`: https://github.com/makerdao/dss-exec-lib/blob/69b658f35d8618272cd139dfc18c5713caf6b96b/src/DssExecLib.sol#L717
+        DssExecLib.setValue(MCD_SPOT, "GUNIV3DAIUSDC1-A", "mat", 10 * RAY);
 
         // Liquidation Penalty (chop): reduce by 13 percentage points, from 13% to 0%
+        DssExecLib.setIlkLiquidationPenalty("GUNIV3DAIUSDC1-A", 0);
 
         // Flat Kick Incentive (tip): reduce by 300, from 300 to 0
+        DssExecLib.setKeeperIncentiveFlatRate("GUNIV3DAIUSDC1-A", 0);
 
         // Proportional Kick Incentive (chip): reduce by 0.1 percentage points, from 0.1% to 0%
+        DssExecLib.setKeeperIncentivePercent("GUNIV3DAIUSDC1-A", 0);
 
         // Auction Price Multiplier (buf): reduce by 3 percentage points, from 105% to 102%
+        DssExecLib.setStartingPriceMultiplicativeFactor("GUNIV3DAIUSDC1-A", 102_00);
 
         // Max Auction Drawdown (cusp): increase by 5 percentage points, from 90% to 95%
+        DssExecLib.setAuctionPermittedDrop("GUNIV3DAIUSDC1-A", 95_00);
 
         // Auction Price Function (step): reduce by 60 seconds, from 120 seconds to 60 seconds
+        DssExecLib.setValue(MCD_CLIP_CALC_GUNIV3DAIUSDC1_A, "step", 60);
 
         // Max Auction Duration (tail): reduce by 9,900 seconds, from 13,200 seconds to 3,300 seconds
+        DssExecLib.setAuctionTimeBeforeReset("GUNIV3DAIUSDC1-A", 3_300);
 
         // Update the value of stopped to 0 on MCD_CLIP_GUNIV3DAIUSDC1_A
+        DssExecLib.setValue(MCD_CLIP_GUNIV3DAIUSDC1_A, "stopped", 0);
 
         // Update GUNIV3DAIUSDC2-A Parameters as follows:
 
         // Liquidation Ratio (mat): increase by 898 percentage points, from 102% to 1000%
+        // Note: We are using low level methods because DssExecLib only allows setting `mat < 1000%`: https://github.com/makerdao/dss-exec-lib/blob/69b658f35d8618272cd139dfc18c5713caf6b96b/src/DssExecLib.sol#L717
+        DssExecLib.setValue(MCD_SPOT, "GUNIV3DAIUSDC2-A", "mat", 10 * RAY);
 
         // Liquidation Penalty (chop): reduce by 13 percentage points, from 13% to 0%
+        DssExecLib.setIlkLiquidationPenalty("GUNIV3DAIUSDC2-A", 0);
 
         // Flat Kick Incentive (tip): reduce by 300, from 300 to 0
+        DssExecLib.setKeeperIncentiveFlatRate("GUNIV3DAIUSDC2-A", 0);
 
         // Proportional Kick Incentive (chip): reduce by 0.1 percentage points, from 0.1% to 0%
+        DssExecLib.setKeeperIncentivePercent("GUNIV3DAIUSDC2-A", 0);
 
         // Auction Price Multiplier (buf): reduce by 3 percentage points, from 105% to 102%
+        DssExecLib.setStartingPriceMultiplicativeFactor("GUNIV3DAIUSDC2-A", 102_00);
 
         // Max Auction Drawdown (cusp): increase by 5 percentage points, from 90% to 95%
+        DssExecLib.setAuctionPermittedDrop("GUNIV3DAIUSDC2-A", 95_00);
 
         // Auction Price Function (step): reduce by 60 seconds, from 120 seconds to 60 seconds
+        DssExecLib.setValue(MCD_CLIP_CALC_GUNIV3DAIUSDC2_A, "step", 60);
 
         // Max Auction Duration (tail): reduce by 9,900 seconds, from 13,200 seconds to 3,300 seconds
+        DssExecLib.setAuctionTimeBeforeReset("GUNIV3DAIUSDC2-A", 3_300);
 
         // Update the value of stopped to 0 on MCD_CLIP_GUNIV3DAIUSDC2_A
+        DssExecLib.setValue(MCD_CLIP_GUNIV3DAIUSDC2_A, "stopped", 0);
 
         // ---------- Whitelist Keel SubProxy to send cross-chain messages to Solana ----------
         // Forum: https://forum.sky.money/t/executive-inclusion-whitelisting-the-keel-subproxy-to-send-cross-chain-messages-to-solana/27447
         // Atlas: https://sky-atlas.io/#A.6.1.1.3.2.6.1.2.1.1.4.3
 
         // Allowlist Keel SubProxy for SVM Controller program at ALM1JSnEhc5PkNecbSZotgprBuJujL5objTbwGtpTgTd
+        GovernanceOAppSenderLike(LZ_GOV_SENDER).setCanCallTarget(
+            KEEL_SUBPROXY,
+            SOL_EID,
+            SVM_CONTROLLER,
+            true
+        );
 
         // Allowlist Keel SubProxy for BPF Loader V3 at BPFLoaderUpgradeab1e11111111111111111111111
+        GovernanceOAppSenderLike(LZ_GOV_SENDER).setCanCallTarget(
+            KEEL_SUBPROXY,
+            SOL_EID,
+            BPF_LOADER,
+            true
+        );
 
         // ---------- Adjust DC-IAM Parameters for Grove ----------
         // Forum: https://forum.sky.money/t/jan-15-2026-parameter-changes-grove-allocator-vault/27595
         // Atlas: https://vote.sky.money/polling/QmYmaVVc#poll-detail
 
-        // Increase ALLOCATOR-BLOOM-A DC-IAM `gap` by 200 million USDS, from 50 million USDS to 250 million USDS
-
-        // Increase ALLOCATOR-BLOOM-A DC-IAM `line `by 2.5 billion USDS, from 2.5 billion USDS to 5 billion USDS
+        // Note: use DssExecLib.setIlkAutoLineParameters() to update multiple ALLOCATOR-BLOOM-A DC-IAM parameters at the same time:
+        DssExecLib.setIlkAutoLineParameters({
+            _ilk: "ALLOCATOR-BLOOM-A",
+            // Increase ALLOCATOR-BLOOM-A DC-IAM `gap` by 200 million USDS, from 50 million USDS to 250 million USDS
+            _gap: 250 * MILLION,
+            // Increase ALLOCATOR-BLOOM-A DC-IAM `line `by 2.5 billion USDS, from 2.5 billion USDS to 5 billion USDS
+            _amount: 5000 * MILLION,
+            // Note: Keep `ttl` unchanged at 86,400 seconds
+            _ttl: 86400 seconds
+        });
 
         // ---------- Delegate Compensation for December ----------
         // Forum: https://forum.sky.money/t/december-2025-ranked-delegate-compensation/27605
         // Atlas: https://sky-atlas.io/#A.1.5
 
         // Transfer 4,000 USDS to AegisD at 0x78C180CF113Fe4845C325f44648b6567BC79d6E0
+        _transferUsds(AEGIS_D, 4_000 * WAD);
 
         // Transfer 4,000 USDS to BLUE at 0xb6C09680D822F162449cdFB8248a7D3FC26Ec9Bf
+        _transferUsds(BLUE, 4_000 * WAD);
 
         // Transfer 4,000 USDS to Bonapublica at 0x167c1a762B08D7e78dbF8f24e5C3f1Ab415021D3
+        _transferUsds(BONAPUBLICA, 4_000 * WAD);
 
         // Transfer 4,000 USDS to Cloaky at 0x9244F47D70587Fa2329B89B6f503022b63Ad54A5
+        _transferUsds(CLOAKY_2, 4_000 * WAD);
 
         // Transfer 3,723 USDS to Tango at 0xB2B86A130B1EC101e4Aed9a88502E08995760307
+        _transferUsds(TANGO, 3_723 * WAD);
 
         // Transfer 1,032 USDS to Sky Staking at 0x05c73AE49fF0ec654496bF4008d73274a919cB5C
+        _transferUsds(SKY_STAKING, 1_032 * WAD);
 
         // ---------- Whitelist Spark Proxy Spell in Starguard ----------
         // Forum: https://forum.sky.money/t/january-15-2026-proposed-changes-to-spark-for-upcoming-spell/27585
@@ -143,17 +260,36 @@ contract DssSpellAction is DssAction {
         // Poll: https://snapshot.box/#/s:sparkfi.eth/proposal/0x7eb3a86a4da21475e760e2b2ed0d82fd72bbd4d33c99a0fbedf3d978e472f362
 
         // Whitelist the Spark Proxy Spell deployed to 0xCE352d9429A5e10b29D3d610C7217f9333e04aB4 with codehash 0x10d1055c82acd9d6804cfb64a80decf3880a257b8af6adad603334325d2586ed; direct execution: no in Spark Starguard
+        StarGuardLike(SPARK_STARGUARD).plot(SPARK_SPELL, SPARK_SPELL_HASH);
 
         // ---------- Whitelist Grove Proxy Spell in Starguard ----------
         // Forum: https://forum.sky.money/t/january-15th-2026-proposed-changes-to-grove-for-upcoming-spell/27570
 
         // Whitelist the Grove Proxy Spell deployed to 0x90230A17dcA6c0b126521BB55B98f8C6Cf2bA748 with codehash 0x9317fd876201f5a1b08658b47a47c8980b8c8aa7538e059408668b502acfa5fb; direct execution: no in Grove Starguard 
+        StarGuardLike(GROVE_STARGUARD).plot(GROVE_SPELL, GROVE_SPELL_HASH);
 
         // ---------- Whitelist Keel Proxy Spell in Starguard ----------
         // Forum: https://forum.sky.money/t/january-15-2026-prime-technical-scope-parameter-change-for-upcoming-spell/27567
         // Poll: https://vote.sky.money/polling/QmdomJ7o
 
         // Whitelist the Keel Proxy Spell deployed to 0x10AF705fB80bc115FCa83a6B976576Feb1E1aaca with codehash 0xa231c2a3fa83669201d02335e50f6aa379a6319c5972cc046b588c08d91fd44d; direct execution: no in Keel Starguard 
+        StarGuardLike(KEEL_STARGUARD).plot(KEEL_SPELL, KEEL_SPELL_HASH);
+    }
+
+    // ---------- Helper Functions ----------
+
+    /// @notice Wraps the operations required to transfer USDS from the surplus buffer.
+    /// @param usr The USDS receiver.
+    /// @param wad The USDS amount in wad precision (10 ** 18).
+    function _transferUsds(address usr, uint256 wad) internal {
+        // Note: Enforce whole units to avoid rounding errors
+        require(wad % WAD == 0, "transferUsds/non-integer-wad");
+        // Note: DssExecLib currently only supports Dai transfers from the surplus buffer.
+        DssExecLib.sendPaymentFromSurplusBuffer(address(this), wad / WAD);
+        // Note: Approve DAI_USDS for the amount sent to be able to convert it.
+        GemAbstract(DAI).approve(DAI_USDS, wad);
+        // Note: Convert Dai to USDS for `usr`.
+        DaiUsdsLike(DAI_USDS).daiToUsds(usr, wad);
     }
 }
 
