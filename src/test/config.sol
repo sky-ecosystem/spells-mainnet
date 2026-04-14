fa0e0406d52552cdb9767026cfd98afdd3349355
SidestreamStrongStrawberry <strongstrawberry@sidestream.tech>
2026-04-14 11:26:21 +0200

    Base spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -142,9 +142,9 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:         address(0x70Da14478667C08320Ef65506063abba84B6990f), // populate with deployed spell if deployed
-            deployed_spell_created: 1775822267,          // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
-            deployed_spell_block:   24849172,          // use `make deploy-info tx=<deployment-tx>` to obtain the block number
+            deployed_spell:         address(0), // populate with deployed spell if deployed
+            deployed_spell_created: 0,          // use `make deploy-info tx=<deployment-tx>` to obtain the timestamp
+            deployed_spell_block:   0,          // use `make deploy-info tx=<deployment-tx>` to obtain the block number
             previous_spells:        prevSpells, // older spells to ensure are executed first
             office_hours_enabled:   true,       // true if officehours is expected to be enabled in the spell
             expiration_threshold:   30 days     // Amount of time before spell expires
