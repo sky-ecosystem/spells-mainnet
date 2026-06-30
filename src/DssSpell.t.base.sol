867204d747f03d08868f288bf66f5b5a07a43bc5
Riccardo Persiani <r.persiani92@gmail.com>
2026-06-30 12:39:41 +0200

    fix: use Vat[\'LSEV2-SKY-A\'].line instead of stusds.line

--- src/DssSpell.t.base.sol
+++ src/DssSpell.t.base.sol
@@ -1460,7 +1460,8 @@ contract DssSpellTestBase is Config, DssTest {
         // TODO: consider a buffer for fee accrual
         assertTrue(vat.debt() + sums[1] <= vat.Line(), "TestError/vat-Line-1");
 
-        uint256 stusdsAvailableLineIncrease = rateSetter.maxLine() - stusds.line();
+        (,,, uint256 stusdsVatLine,) = vat.ilks(stusds.ilk());
+        uint256 stusdsAvailableLineIncrease = rateSetter.maxLine() - stusdsVatLine;
         uint256 minimumGlobalLine = sums[0] + stusdsAvailableLineIncrease;
 
         // Enforce the global Line also falls between the minimum global Line and that minimum global Line + offset.
