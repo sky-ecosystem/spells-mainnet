b63704aaa2adaac5a0b9703539e0415e9d1ab522
SidestreamFrostyFig <frostyfig@sidestream.tech>
2026-07-02 16:53:45 +0200

    Mainnet spell 2026-07-02 (#556)

--- archive/2026-07-02-DssSpell/dependencies/endgame-toolkit/VestedRewardsDistributionInit.sol
+++ archive/2026-07-02-DssSpell/dependencies/endgame-toolkit/VestedRewardsDistributionInit.sol
@@ -1,4 +1,3 @@
-// SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 // This program is free software: you can redistribute it and/or modify
@@ -14,16 +13,18 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.16;
 
-struct AllocatorSharedInstance {
-    address oracle;
-    address roles;
-    address registry;
+struct VestedRewardsDistributionInitParams {
+    uint256 vestId;
 }
 
-struct AllocatorIlkInstance {
-    address owner;
-    address vault;
-    address buffer;
+library VestedRewardsDistributionInit {
+    function init(address dist, VestedRewardsDistributionInitParams memory p) internal {
+        VestedRewardsDistributionLike(dist).file("vestId", p.vestId);
+    }
+}
+
+interface VestedRewardsDistributionLike {
+    function file(bytes32 what, uint256 data) external;
 }
